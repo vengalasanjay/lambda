ec0ffbbba47472beda6a2717f920af0c4dabe73f
vengalasanjay <112609021+vengalasanjay@users.noreply.github.com>
2024-04-29 14:44:19 +0530

    Update mypythoncode.py

--- mypythoncode.py
+++ mypythoncode.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print('This is pushed from GitHub')
+    print('This code is pushed from GitHub')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
